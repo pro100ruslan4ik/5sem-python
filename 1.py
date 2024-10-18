f3125d9855aaddaf72bb39691eb9b92ffad453f3
pro100ruslan4ik <haertdionov.ruslan@gmail.com>
2024-10-18 16:05:16 +0500

    init

---
+++ 1.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt  
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np  
+
+x = np.linspace(-10, 10, 4000)
+y_positive = np.sqrt(x**2 - 1)
+y_negative = -np.sqrt(x**2 - 1)
+
+plt.plot(x, y_positive, x, y_negative)  
+plt.title('График функции y = ±√(x² - 1)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+plt.show()  
